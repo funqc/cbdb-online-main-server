502c2a6a603b0ba58421e3607aa8652d82b412b9
jianan <s8714081@hotmail.com>
2024-10-09 00:26:09 +0800

    20241008Update packages.json (#276)
    Co-authored-by: Hongsu Wang <sudospace@gmail.com>

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,7 +7,12 @@
 
 require('./bootstrap');
 
+/*
+20240913 提升vue各套件版本至安全版本 by ja
 window.Vue = require('vue');
+*/
+window.Vue = require('vue').default;
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,39 +20,39 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
-Vue.component('name-list', require('./components/NameList.vue'));
-Vue.component('address-code-list', require('./components/AddrCodeList.vue'));
-Vue.component('altname-code-list', require('./components/AltnameCodeList.vue'));
-Vue.component('appoint-code-list', require('./components/AppointCodeList.vue'));
-Vue.component('text-code-list', require('./components/TextCodeList.vue'));
-Vue.component('text-instance-data-list', require('./components/TextInstanceDataList.vue'));
-Vue.component('addr-belongs-data-list', require('./components/AddrBelongsDataList.vue'));
-Vue.component('addr-code-list', require('./components/Addr2CodeList.vue'));
-Vue.component('office-code-list', require('./components/OfficeCodeList.vue'));
-Vue.component('social-institution-code-list', require('./components/SocialInstitutionCodeList.vue'));
-Vue.component('codebox', require('./components/codebox.vue'));
+Vue.component('example', require('./components/Example.vue').default);
+Vue.component('name-list', require('./components/NameList.vue').default);
+Vue.component('address-code-list', require('./components/AddrCodeList.vue').default);
+Vue.component('altname-code-list', require('./components/AltnameCodeList.vue').default);
+Vue.component('appoint-code-list', require('./components/AppointCodeList.vue').default);
+Vue.component('text-code-list', require('./components/TextCodeList.vue').default);
+Vue.component('text-instance-data-list', require('./components/TextInstanceDataList.vue').default);
+Vue.component('addr-belongs-data-list', require('./components/AddrBelongsDataList.vue').default);
+Vue.component('addr-code-list', require('./components/Addr2CodeList.vue').default);
+Vue.component('office-code-list', require('./components/OfficeCodeList.vue').default);
+Vue.component('social-institution-code-list', require('./components/SocialInstitutionCodeList.vue').default);
+Vue.component('codebox', require('./components/codebox.vue').default);
 
-Vue.component('select-vue', require('./components/Select.vue'));
-Vue.component('select2-vue', require('./components/Select2Vue.vue'));
-Vue.component('select2', require('./components/Select2.vue'));
-Vue.component('select2-addr', require('./components/Select2Addr.vue'));
+Vue.component('select-vue', require('./components/Select.vue').default);
+Vue.component('select2-vue', require('./components/Select2Vue.vue').default);
+Vue.component('select2', require('./components/Select2.vue').default);
+Vue.component('select2-addr', require('./components/Select2Addr.vue').default);
 
 Vue.component(
     'passport-clients',
-    require('./components/passport/Clients.vue')
+    require('./components/passport/Clients.vue').default
 );
 
 Vue.component(
     'passport-authorized-clients',
-    require('./components/passport/AuthorizedClients.vue')
+    require('./components/passport/AuthorizedClients.vue').default
 );
 
 Vue.component(
     'passport-personal-access-tokens',
-    require('./components/passport/PersonalAccessTokens.vue')
+    require('./components/passport/PersonalAccessTokens.vue').default
 );
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
 });
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,3 @@
-let mix = require('laravel-mix');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -10,7 +8,40 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+ 
+let mix = require('laravel-mix');
+  
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .options({ processCssUrls: false });
+   .vue({ version: 2 });
+
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  }
+};
+
