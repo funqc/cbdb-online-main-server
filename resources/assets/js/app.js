ec0c98fea9c0ded725c2ffd9df64217e41ce4cf6
jianan <s8714081@hotmail.com>
2024-11-06 01:28:00 +0800

    20241105 Bump vue from 2.7.16 to 3.0.0問題修正 (#284)

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,58 +1,51 @@
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import './bootstrap';
 
-require('./bootstrap');
+// 20241105升級Vue 3進行修改
+// 確保所有組件都已經正確導入和註冊，在Vue 3中，使用 import 而不是 require。
 
-/*
-20240913 提升vue各套件版本至安全版本 by ja
-window.Vue = require('vue');
-*/
-window.Vue = require('vue').default;
+import { createApp } from 'vue';
+import Example from './components/Example.vue';
+import NameList from './components/NameList.vue';
+import AddrCodeList from './components/AddrCodeList.vue';
+import AltnameCodeList from './components/AltnameCodeList.vue';
+import AppointCodeList from './components/AppointCodeList.vue';
+import TextCodeList from './components/TextCodeList.vue';
+import TextInstanceDataList from './components/TextInstanceDataList.vue';
+import AddrBelongsDataList from './components/AddrBelongsDataList.vue';
+import Addr2CodeList from './components/Addr2CodeList.vue';
+import OfficeCodeList from './components/OfficeCodeList.vue';
+import SocialInstitutionCodeList from './components/SocialInstitutionCodeList.vue';
+import Codebox from './components/codebox.vue';
+import SelectVue from './components/Select.vue';
+import Select2Vue from './components/Select2Vue.vue';
+import Select2 from './components/Select2.vue';
+import Select2Addr from './components/Select2Addr.vue';
+import PassportClients from './components/passport/Clients.vue';
+import PassportAuthorizedClients from './components/passport/AuthorizedClients.vue';
+import PassportPersonalAccessTokens from './components/passport/PersonalAccessTokens.vue';
 
+const app = createApp({
+    components: {
+        'example': Example,
+        'name-list': NameList,
+        'address-code-list': AddrCodeList,
+        'altname-code-list': AltnameCodeList,
+        'appoint-code-list': AppointCodeList,
+        'text-code-list': TextCodeList,
+        'text-instance-data-list': TextInstanceDataList,
+        'addr-belongs-data-list': AddrBelongsDataList,
+        'addr-code-list': Addr2CodeList,
+        'office-code-list': OfficeCodeList,
+        'social-institution-code-list': SocialInstitutionCodeList,
+        'codebox': Codebox,
+        'select-vue': SelectVue,
+        'select2-vue': Select2Vue,
+        'select2': Select2,
+        'select2-addr': Select2Addr,
+        'passport-clients': PassportClients,
+        'passport-authorized-clients': PassportAuthorizedClients,
+        'passport-personal-access-tokens': PassportPersonalAccessTokens
+    }
+}).mount('#app');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example', require('./components/Example.vue').default);
-Vue.component('name-list', require('./components/NameList.vue').default);
-Vue.component('address-code-list', require('./components/AddrCodeList.vue').default);
-Vue.component('altname-code-list', require('./components/AltnameCodeList.vue').default);
-Vue.component('appoint-code-list', require('./components/AppointCodeList.vue').default);
-Vue.component('text-code-list', require('./components/TextCodeList.vue').default);
-Vue.component('text-instance-data-list', require('./components/TextInstanceDataList.vue').default);
-Vue.component('addr-belongs-data-list', require('./components/AddrBelongsDataList.vue').default);
-Vue.component('addr-code-list', require('./components/Addr2CodeList.vue').default);
-Vue.component('office-code-list', require('./components/OfficeCodeList.vue').default);
-Vue.component('social-institution-code-list', require('./components/SocialInstitutionCodeList.vue').default);
-Vue.component('codebox', require('./components/codebox.vue').default);
-
-Vue.component('select-vue', require('./components/Select.vue').default);
-Vue.component('select2-vue', require('./components/Select2Vue.vue').default);
-Vue.component('select2', require('./components/Select2.vue').default);
-Vue.component('select2-addr', require('./components/Select2Addr.vue').default);
-
-Vue.component(
-    'passport-clients',
-    require('./components/passport/Clients.vue').default
-);
-
-Vue.component(
-    'passport-authorized-clients',
-    require('./components/passport/AuthorizedClients.vue').default
-);
-
-Vue.component(
-    'passport-personal-access-tokens',
-    require('./components/passport/PersonalAccessTokens.vue').default
-);
-
-const app = new Vue({
-    el: '#app',
-});
